8a957abaf553986b84e43b20fc225892be011240
ahodzic11 <ahodzic11@etf.unsa.ba>
2022-12-01 00:54:57 +0100

    Get All Tags endpoint implemented.

--- src/api/blogs/blogs.routes.ts
+++ src/api/blogs/blogs.routes.ts
@@ -7,6 +7,7 @@ const router = Router();
 router.post('/', BlogController.createBlog)
 router.put('/:slug', BlogController.updateBlog)
 router.delete('/:slug', BlogController.deleteBlog)
+router.get('')
 
 
 export default router;
