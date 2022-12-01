3f4b7e2817ffb74413e6b41290a01ec16351b997
ahodzic11 <ahodzic11@etf.unsa.ba>
2022-12-01 13:45:31 +0100

    Get Comments from a blog post endpoint implemented

--- src/api/blogs/comments.controller.ts
+++ src/api/blogs/comments.controller.ts
@@ -1,5 +1,5 @@
 import { Response, Request, NextFunction } from 'express';
-import { Comment, CommentRequestBody } from './comment.model';
+import { Comment, CommentRequestBody, MultipleComments, SingleCommentData } from './comment.model';
 import { ParamsWithSlug } from '../../interfaces/ParamsWithSlug';
 import { Blog, Blogs, BlogWithId } from './blog.model';
 
@@ -17,7 +17,7 @@ export async function addComment(req: Request<ParamsWithSlug, Comment, CommentRe
         const result = await Blogs.findOneAndUpdate({
             "blogPost.slug": req.params.slug,
         },{
-            $push: newComment
+            $push: { "comments": newComment.comment}
         }, {
             returnDocument: 'after',
         });
@@ -28,6 +28,43 @@ export async function addComment(req: Request<ParamsWithSlug, Comment, CommentRe
         }
         res.json(newComment);
     }catch(error){
+        next(error);
+    }
+}
+
+export async function getComments(req: Request<ParamsWithSlug, MultipleComments, {}>, res: Response<MultipleComments>, next: NextFunction){
+    try{
+        const result = await Blogs.findOne({
+            "blogPost.slug": req.params.slug,
+        });
+        if(!result){
+            res.status(404);
+            throw new Error(`Blog with id "${req.params.slug}" not found.`);
+        }
+        let allBlogComments: MultipleComments = {
+            comments: []
+        }
+        result.comments.forEach(comment => {
+            allBlogComments.comments.push(comment)
+        });
+        res.json(allBlogComments);
+    }catch (error) {
+        next(error);
+    }
+}
+
+
+export async function deleteComment(req: Request<ParamsWithSlug, {}, {}>, res: Response<{}>, next:NextFunction){
+    try{
+        const result = await Blogs.findOneAndDelete({
+            "blogPost.slug": req.params.slug,
+        });
+        if(!result.value){
+            res.status(404);
+            throw new Error(`Blog with id "${req.params.slug}" not found.`)
+        }
+        res.status(204).end();
+    } catch (error) {
         next(error);
     }
 }
--- src/api/blogs/comments.routes.ts
+++ src/api/blogs/comments.routes.ts
@@ -5,7 +5,7 @@ import * as CommentController from './comments.controller'
 const router = Router();
 
 router.post('/:slug/comments', CommentController.addComment)
-
-
+router.get('/:slug/comments', CommentController.getComments)
+router.delete('/:slug/comments/:id', CommentController.deleteComment)
 
 export default router;
